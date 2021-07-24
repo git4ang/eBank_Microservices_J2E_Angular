13c4482dd3967d55e4838dfc5d74ad5de3edcec6
ANG <code4ang@gmail.com>
2021-07-24 20:36:37 +0200

    M02-eureka-server: creation & implementation of module eureka-server

---
+++ M02-eureka-server/src/test/java/ang/neggaw/eureka/M02EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ang.neggaw.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M02EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
