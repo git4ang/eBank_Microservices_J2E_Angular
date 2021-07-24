49f453c745f3651035eeb09be53c55eb85a60f15
ANG <code4ang@gmail.com>
2021-07-24 19:43:09 +0200

    M01-config-server: creation & implementation of module config-server

---
+++ M01-config-server/src/main/java/ang/neggaw/config/M01ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package ang.neggaw.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@EnableConfigServer
+@SpringBootApplication
+public class M01ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(M01ConfigServerApplication.class, args);
+    }
+}
---
+++ M01-config-server/src/test/java/ang/neggaw/config/M01ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ang.neggaw.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M01ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
