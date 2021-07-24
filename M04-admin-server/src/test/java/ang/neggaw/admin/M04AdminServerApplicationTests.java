eb63a882bc54e20d212bbc497663528be15ed0fe
ANG <code4ang@gmail.com>
2021-07-24 20:55:05 +0200

    M04-admin-server: creation & implementation of module admin-server

---
+++ M04-admin-server/src/test/java/ang/neggaw/admin/M04AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ang.neggaw.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M04AdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
